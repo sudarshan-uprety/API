dec7fade9b00649abcbc752bc639a256e4e71e5c
Sudarshan uprety <sudarshan.uprety@bottle.com.np>
2023-03-22 20:09:09 +0545

    commeted database features and all

--- modles.py
+++ modles.py
@@ -1,17 +1,17 @@
-from mongoengine import *
-connect('mydatabase', host='localhost', port=27017)
+# from mongoengine import *
+# connect('mydatabase', host='localhost', port=27017)
 
-# disconnect()
+# # disconnect()
 
-class User(Document):
-    email=EmailField(required=True,unique=True)
-    first_name=StringField(max_length=30,required=True)
-    last_name=StringField(max_length=30,required=True)
-    password = StringField(required=True,password=True)
+# class User(Document):
+#     email=EmailField(required=True,unique=True)
+#     first_name=StringField(max_length=30,required=True)
+#     last_name=StringField(max_length=30,required=True)
+#     password = StringField(required=True,password=True)
 
-    meta = {
-        'db': 'mydatabase',
-        'collection': 'User',
-        'strict': False
+#     meta = {
+#         'db': 'mydatabase',
+#         'collection': 'User',
+#         'strict': False
 
-    }
+#     }
