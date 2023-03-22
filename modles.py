d49aca8a93ace7f407a8c4887665fc69d4b09e43
Sudarshan uprety <sudarshan.uprety@bottle.com.np>
2023-03-22 19:45:59 +0545

    uploading

---
+++ modles.py
@@ -0,0 +1,17 @@
+from mongoengine import *
+connect('mydatabase', host='localhost', port=27017)
+
+# disconnect()
+
+class User(Document):
+    email=EmailField(required=True,unique=True)
+    first_name=StringField(max_length=30,required=True)
+    last_name=StringField(max_length=30,required=True)
+    password = StringField(required=True,password=True)
+
+    meta = {
+        'db': 'mydatabase',
+        'collection': 'User',
+        'strict': False
+
+    }
---
+++ user.py
@@ -0,0 +1,28 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler 
+import json 
+
+from modles import connect,disconnect,User
+
+
+class UserFunctions(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        if self.path=='/create' and self.command=='POST':
+            content_length=int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length).decode()
+            data=json.loads(body)
+            if data["email"] == "" or data["password"] == "" or data["first_name"]=="" or data["last_name"]=="" or data["password"]=="":
+                self.send_error(400,"Fields can not be empty")
+            disconnect(alias='default')
+            connect()
+            user=User(email=data["email"],first_name=data["first_name"],last_name=data["last_name"],password=data["password"])
+            user.save()
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            response = {"message": "User created successfully"}
+            self.wfile.write(json.dumps(response).encode())
+
+    
+httpd = HTTPServer(('localhost', 4000), UserFunctions) 
+httpd.serve_forever()
